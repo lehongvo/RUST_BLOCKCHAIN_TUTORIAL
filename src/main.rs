f8edc062fea0eb930d8b133f58a5aaa2e7f2cf15
“VoLeHong” <volh@bap.jp>
2024-02-16 11:48:40 +0700

    [Volh][Blockchain][Rust][data type: String]

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,23 @@
 // }
 
 
-fn print_stg(s: &str) {
-    println!("print: {}", s);
+// fn print_stg(s: &str) {
+//     println!("print: {}", s);
+// }
+
+// fn main() {
+//     let my_str: &str = "Hello, Rust";
+//     let new_sth = my_str.to_owned() + "....No";
+//     println!("new_sth: {}", new_sth)
+// }
+
+fn return_stg(mut s: String) -> String {
+    s = s + "....OMG";
+    return s;
 }
 
 fn main() {
-    let my_str: &str = "Hello, Rust";
-    let new_sth = my_str.to_owned() + "....No";
-    println!("new_sth: {}", new_sth)
+    let input_string = String::from("Initial String");
+    let current_string = String::from(return_stg(input_string));
+    println!("current_string: {}", current_string);
 }
