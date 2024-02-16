a955eb94a3ba6fbc0e2bf444e9ef8e569934e8cc
“VoLeHong” <volh@bap.jp>
2024-02-16 10:12:34 +0700

    [Volh][Blockchain][Rust][LowerCase + UpperCase]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let mut string_value: &str = "Hello, world!";
-    let binding = (string_value.to_owned() + "world!");
-    println!("binding: {}", binding);
+    let string_value: &str = "Hello, world!";
+    let mut binding = string_value.to_owned() + "world!";
+    let bindingUp: &str = &binding.to_uppercase();
+    let bindingLow: &str = &binding.to_lowercase();
+    println!("bindingUp: {}", bindingUp);
+    println!("bindingLow: {}", bindingLow);
 }
