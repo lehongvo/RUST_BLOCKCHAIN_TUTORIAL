de7e734e5f016f99efe570174c77154ee15e0b29
“VoLeHong” <volh@bap.jp>
2024-02-26 09:46:54 +0700

    [VoLH][Blockcchain][Rust][Storing Keys with Associated Values in Hash Maps]

--- store_vector/src/main.rs
+++ store_vector/src/main.rs
@@ -59,28 +59,417 @@
 
 // }
 
-fn main() {
-    // let my_str: &str = "Hello, world!";
-    // let my_string: String = String::from("Hello, world");
-    // println!("Value is {}", my_str);
-    // println!("Value is {}", my_string);
+// fn main() {
+//     // let my_str: &str = "Hello, world!";
+//     // let my_string: String = String::from("Hello, world");
+//     // println!("Value is {}", my_str);
+//     // println!("Value is {}", my_string);
 
-    // let mut empty_string = String::new();
+//     // let mut empty_string = String::new();
 
-    // let data = "Initial contents";
-    // let data_to_string = data.to_string();
+//     // let data = "Initial contents";
+//     // let data_to_string = data.to_string();
 
-    // let mut my_string = String::from("foo");
-    // my_string.push_str("Hi");
-    // println!("my_string {}", my_string)
+//     // let mut my_string = String::from("foo");
+//     // my_string.push_str("Hi");
+//     // println!("my_string {}", my_string)
 
-    // let s1 = String::from("Hello,!");
-    // let s2 = String::from("world!");
-    // let s3 = s1 + &s2;
-    // println!("Values is s3 {}", s3);
+//     // let s1 = String::from("Hello,!");
+//     // let s2 = String::from("world!");
+//     // let s3 = s1 + &s2;
+//     // println!("Values is s3 {}", s3);
 
-    let hello = "Здравствуйте";
-    for value in hello.chars() {
-        println!("Value is {}", value);
+//     let hello = "Здравствуйте";
+//     for value in hello.chars() {
+//         println!("Value is {}", value);
+//     }
+//  }
+// use std::collections::HashMap;
+// fn main() {
+//     let mut scores: HashMap<String, u8> = HashMap::new();
+//     scores.insert("Blue".to_string(), 10);
+//     scores.insert("Yellow".to_string(), 50);
+
+//     let team_name = String::from("Blue");
+//     let value = scores.get(&team_name).copied().unwrap_or(0);
+// println!("Score for {team_name} is {value}");
+
+// for (key, value) in &scores {
+//     println!("Key is: {}, Value is: {:?}", key, value);
+// }
+// scores.insert(String::from("Blue"), 25);
+// println!("Score is {:?}", scores);
+
+// scores.entry(String::from("Green")).or_insert(30);
+// println!("Score is {:?}", scores);
+
+// let mut word_count: HashMap<String, u8> = HashMap::new();
+// let text = "Hello world wonderful world";
+// let spit_text: Vec<&str> = text.split(' ').collect();
+
+// for value in &spit_text {
+//     println!("Value is: {:?}", value);
+//     let count = word_count.entry(value.to_string()).or_insert(0);
+//     *count += 1;
+// }
+// println!("Value is {:?}", word_count);
+
+// let text = "Hello world wonderful world";
+// let mut word_count: HashMap<String, u8> = HashMap::new();
+
+// let split_text = text.split_whitespace();
+// for value in split_text {
+//     let count = word_count.entry(String::from(value)).or_insert(0);
+//     *count += 1;
+// }
+// println!("Value is {:?}", word_count);
+// }
+
+// fn calculate_median(numbers: &Vec<i32>) -> f64 {
+//     let mut numbers_clone = numbers.clone();
+//     numbers_clone.sort_by(|a, b| a.cmp(b));
+//     let length = numbers_clone.len();
+//     if length % 2 == 0 {
+//         let middle1: i32 = numbers_clone[length / 2 - 1];
+//         let middle2: i32 = numbers_clone[length / 2];
+//         let value: f64 = ((middle1 + middle2) / 2) as f64;
+//         return value;
+//     } else {
+//         let value = numbers_clone[length / 2] as f64;
+//         return value;
+//     }
+// }
+
+/*
+let mut mapping_hashmap: HashMap<i32, i32> = HashMap::new();
+    for &value in numbers {
+        let count = mapping_hashmap.entry(value).or_insert(0);
+        *count += 1;
     }
- }
+
+    let (result_value, result_count) = mapping_hashmap
+        .iter()
+        .max_by_key(|&(_, count)| count)
+        .unwrap_or((&0, &0));
+
+    return (*result_value, *result_count)
+*/
+// fn same_max_number(numbers: &Vec<i32>) -> (i32, i32) {
+//     let mut new_mapping: HashMap<i32, i32> = HashMap::new();
+//     for number in numbers  {
+//         let count = new_mapping.entry(*number).or_insert(0);
+//         *count += 1;
+//     }
+
+//     let (result_value, result_count) = new_mapping.iter().max_by_key(|&(_, count)| count).unwrap_or((&0, &0));
+//     return (*result_value, *result_count);
+// }
+
+// fn main() {
+//     let numbers = vec![8, 8, 8, 8, 8, 1, 2, 3, 3, 5, 5, 1, 1, 7, 2, 2, 1];
+//     // let value_median = calculate_median(&numbers);
+//     // println!("value_median is {:?}", value_median)
+//     let same_max_number = same_max_number(&numbers);
+//     println!("Same max number is {:?}", same_max_number)
+// }
+
+// use std::collections::HashMap;
+
+// trait Office {
+//     fn new() -> Self
+//     where
+//         Self: Sized;
+
+//     fn add_enployee(&mut self, name: &str, department: &str);
+
+//     fn get_department_employees(&self, department: &str) -> Option<&Vec<String>>;
+
+//     fn get_all_employees(&self) -> Vec<String>;
+// }
+
+// #[derive(Debug)]
+// struct Company {
+//     employee_department: HashMap<String, String>,
+//     department_employees: HashMap<String, Vec<String>>,
+// }
+
+// impl Office for Company {
+//     fn new() -> Company {
+//         let company: Company = Company {
+//             employee_department: HashMap::new(),
+//             department_employees: HashMap::new(),
+//         };
+//         return company;
+//     }
+
+//     fn add_enployee(&mut self, name: &str, department: &str) {
+//         self.employee_department
+//             .insert(name.to_string(), department.to_string());
+
+//         let department_employees = self.department_employees
+//             .entry(department.to_string())
+//             .or_insert(vec![]);
+//         department_employees.push(name.to_string());
+//     }
+
+//     fn get_department_employees(&self, department: &str) -> Option<&Vec<String>> {
+//         let department_employees: Option<&Vec<String>> = self.department_employees.get(department);
+//         return department_employees;
+//     }
+
+//     fn get_all_employees(&self) -> Vec<String> {
+//         let mut all_employees: Vec<String> = self.employee_department.keys().cloned().collect();
+//         all_employees.sort();
+//         return all_employees;
+//     }
+// }
+// fn main() {
+//     let mut company: Company = Company::new();
+//     let department_employees_before = company.get_department_employees( "hr");
+//     println!("Department is {:?}", department_employees_before);
+
+//     company.add_enployee("Le Hong Vo", "Dev");
+//     company.add_enployee("Le Hong Van", "Dev");
+//     company.add_enployee("Le Hong Vi", "Dev");
+//     company.add_enployee("Mai Thi Van Anh", "Hr");
+//     company.add_enployee("Mai Thi Van van", "Hr");
+//     company.add_enployee("Mai Thi Van Linh", "Hr");
+//     company.add_enployee("Tran Van Dat", "Admin");
+
+//     let department_employees_after = company.get_department_employees( "Hr");
+//     // println!("Department is {:?}", department_employees_before);
+//     match department_employees_after  {
+//         Some(department_employees) => println!("Department is {:?}", department_employees),
+//         None => println!("Department not found")
+//     }
+
+//     let all_employees = company.get_all_employees();
+//     for (index, value) in all_employees.iter().enumerate()  {
+//         println!("Employee at {index} has name {value}");
+//     }
+// }
+
+// use std::collections::HashMap;
+
+// trait Office {
+//     fn new() -> Self
+//     where
+//         Self: Sized;
+
+//     fn add_employee(&mut self, name: &str, department: &str);
+
+//     fn get_employees_in_department(&self, department: &str) -> Option<&Vec<String>>;
+
+//     fn get_all_employees(&self) -> Vec<String>;
+// }
+
+// struct Company {
+//     employee_from_department: HashMap<String, String>,
+//     department_from_employees: HashMap<String, Vec<String>>,
+// }
+
+// impl Office for Company {
+//     fn new() -> Company {
+//         let company: Company = Company {
+//             employee_from_department: HashMap::new(),
+//             department_from_employees: HashMap::new(),
+//         };
+//         return company;
+//     }
+
+//     fn add_employee(&mut self, name: &str, department: &str) {
+//         self.employee_from_department
+//             .insert(name.to_string(), department.to_string());
+//         let department_from_employees = self
+//             .department_from_employees
+//             .entry(department.to_string())
+//             .or_insert(vec![]);
+//         department_from_employees.push(name.to_string());
+//     }
+
+//     fn get_employees_in_department(&self, department: &str) -> Option<&Vec<String>> {
+//         let all_employees_in_department = self.department_from_employees.get(&department.to_string());
+//         return all_employees_in_department;
+//     }
+
+//     fn get_all_employees(&self) -> Vec<String> {
+//         let mut all_employees_in_company:  Vec<String> = self.employee_from_department.keys().cloned().collect();
+//         all_employees_in_company.sort();
+//         return all_employees_in_company;
+//     }
+// }
+
+// fn main() {
+//     let mut company: Company = Company::new();
+
+//     let get_employees_in_department_after =  company.get_employees_in_department("Dev");
+//     match get_employees_in_department_after {
+//         Some(value) => println!("Value is {:?}", value),
+//         None => println!("Department not found!")
+//     }
+
+//     let get_all_employees = company.get_all_employees();
+//     println!("get_all_employees is {:?}", get_all_employees);
+
+//     company.add_employee("Le Hong Vo1", "Hr");
+//     company.add_employee("Le Hong Vo2", "Hr");
+//     company.add_employee("Le Hong Vo3", "Hr");
+//     company.add_employee("Le Hong Vo4", "Hr");
+//     company.add_employee("Le Hong Vo5", "Admin");
+//     company.add_employee("Le Hong Vo6", "Admin");
+//     company.add_employee("Le Hong Vo7", "Admin");
+//     company.add_employee("Le Hong Vo8", "Dev");
+//     company.add_employee("Le Hong Vo9", "Dev");
+
+//     let get_employees_in_department_before=  company.get_employees_in_department("Dev");
+//     match get_employees_in_department_before {
+//         Some(value) => println!("Value is {:?}", value),
+//         None => println!("Department not found!")
+//     }
+
+//     let get_all_employees = company.get_all_employees();
+//     println!("get_all_employees is {:?}", get_all_employees);
+// }
+
+// use std::{collections::HashMap, sync::Arc};
+// struct Company {
+//     employee_from_department: HashMap<String, HashMap<String, f64>>,
+//     department_from_employees: HashMap<String, Vec<String>>,
+// }
+
+// impl Company {
+//     fn new_company() -> Company {
+//         let company = Company {
+//             employee_from_department: HashMap::new(),
+//             department_from_employees: HashMap::new(),
+//         };
+//         return company;
+//     }
+
+//     fn add_employee(&mut self, name: &str, department: &str, salary_per_month: f64) {
+//         let department_from_employees = self
+//             .employee_from_department
+//             .entry(name.to_string())
+//             .or_insert(HashMap::new());
+//         department_from_employees.insert(department.to_string(), salary_per_month);
+//     }
+
+//     fn get_salary_per_month_1(&self, name: &str, department: &str) -> Option<f64> {
+//         self.employee_from_department
+//             .get(name)
+//             .and_then(|department_employees| department_employees.get(department))
+//             .map_or(None, |&salary| Some(salary))
+//     }
+
+//     fn get_salary_per_month_2(&self, name: &str, department: &str) -> Option<f64> {
+//         match self.employee_from_department.get(name) {
+//             Some(department_employees) => department_employees.get(department).map(|&salary| salary),
+//             None => None
+//         }
+//     }
+// }
+
+use std::collections::HashMap;
+
+struct Company {
+    employees: HashMap<String, HashMap<String, f64>>,
+    managers: HashMap<String, Vec<String>>,
+}
+
+impl Company {
+    fn new() -> Company {
+        let company = Company {
+            employees: HashMap::new(),
+            managers: HashMap::new(),
+        };
+        return company;
+    }
+
+    fn add_employees(&mut self, name: &str, department: &str, salary: f64) {
+        let employees = self
+            .employees
+            .entry(name.to_string())
+            .or_insert(HashMap::new());
+        employees.insert(department.to_string(), salary);
+
+        let managers = self
+            .managers
+            .entry(department.to_string())
+            .or_insert(vec![]);
+        managers.push(name.to_string());
+    }
+
+    fn get_salary_per_month_1(&self, name: &str, department: &str) -> Option<f64> {
+        self.employees
+            .get(name)
+            .and_then(|department_employees| department_employees.get(department))
+            .map_or(None, |&salary| Some(salary))
+    }
+
+    fn get_salary_per_month_2(&self, name: &str, department: &str) -> Option<f64> {
+        match self.employees.get(name) {
+            Some(department_employees) => {
+                department_employees.get(department).map(|&salary| salary)
+            }
+            None => None,
+        }
+    }
+
+    fn get_all_department(&self) -> Vec<String> {
+        let all_department: Vec<String> = self.managers.keys().cloned().collect();
+        return all_department;
+    }
+
+    fn get_all_employees(&self) -> Vec<String> {
+        let all_employees: Vec<String> = self.employees.keys().cloned().collect();
+        return all_employees;
+    }
+
+    fn get_all_member_in_department(&self, department: &str) -> Option<Vec<String>> {
+        if let Some(members) = self.managers.get(department) {
+            Some(members.clone())
+        } else {
+            None
+        }
+    }
+
+    fn average_salary(&self) -> Option<f64> {
+        let total_salary: f64 = self.employees.values().flat_map(|dept| dept.values()).sum();
+        let total_member = self.employees.len() as f64;
+        if total_salary > 0.0 {
+            let return_value = total_salary / total_member;
+            Some(return_value)
+        } else {
+            None
+        }
+    }
+
+}
+
+fn main() {
+    let mut company: Company = Company::new();
+    company.add_employees("Le Hong Vo1", "Admin", 200000000.01);
+    company.add_employees("Le Hong Vo2", "Admin", 300000000.02);
+    company.add_employees("Le Hong Vo3", "Dev", 400000000.03);
+    company.add_employees("Le Hong Vo4", "Dev", 500000000.04);
+    company.add_employees("Le Hong Vo5", "Dev", 600000000.05);
+    company.add_employees("Le Hong Vo6", "HR", 700000000.04);
+
+    let all_employees: Vec<String> = company.get_all_employees();
+    println!("All employees is {:?}", all_employees);
+
+    let all_department: Vec<String> = company.get_all_department();
+    println!("All employees is {:?}", all_department);
+
+    let all_member_in_department: Option<Vec<String>> = company.get_all_member_in_department("Dev");
+    match all_member_in_department {
+        Some(value) =>  println!("Value is {:?}", value),
+        None => println!("Department not found")
+    }
+
+    let average_salary = company.average_salary();
+    match average_salary {
+        Some(value) => println!("Value is {:?}", value),
+        None => println!("Department not found")
+    }
+}
