9871223f031bb0b5950fb7aa8fe1bc1a062ab55a
Vo Le hong <lehongvi19x@gmail.com>
2024-03-01 13:16:17 +0700

    [VoLH][Blockchain][Rust][A simple Rust library-update-v3]

---
+++ cargo_seting/target/package/cargo_io-0.1.0/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn hello() {
+    println!("Hello from my_library!");
+}
+
+pub fn get_value() -> String {
+   let value = String::from("Hello, world!");
+   return value;
+}
---
+++ cargo_seting/target/package/cargo_io-0.1.0/target/debug/deps/my_library-cdab0e23eb3eb682.d
@@ -0,0 +1,7 @@
+/Users/nguyenductrang/Desktop/Contract/RUST_BLOCKCHAIN_TUTORIAL/cargo_seting/target/package/cargo_io-0.1.0/target/debug/deps/libmy_library-cdab0e23eb3eb682.rmeta: src/lib.rs
+
+/Users/nguyenductrang/Desktop/Contract/RUST_BLOCKCHAIN_TUTORIAL/cargo_seting/target/package/cargo_io-0.1.0/target/debug/deps/libmy_library-cdab0e23eb3eb682.rlib: src/lib.rs
+
+/Users/nguyenductrang/Desktop/Contract/RUST_BLOCKCHAIN_TUTORIAL/cargo_seting/target/package/cargo_io-0.1.0/target/debug/deps/my_library-cdab0e23eb3eb682.d: src/lib.rs
+
+src/lib.rs:
