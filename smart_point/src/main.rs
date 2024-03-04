f2ff2729b755b2c2ecc25ebdd23ae9e1efc2e2be
RajSu <lehongvi19x@gmail.com>
2024-03-04 10:37:56 +0700

    [VolH][Blockchain][count after b creation]

--- smart_point/src/main.rs
+++ smart_point/src/main.rs
@@ -93,9 +93,8 @@
 //     let value = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Nil))))))));
 //     println!("Value is {:?}", value);
 // }
-
 // #[derive(Debug)]
-struct MyBox<T>(T);
+// struct MyBox<T>(T);
 
 // impl<T> MyBox<T> {
 //     fn new(x: T) -> MyBox<T> {
@@ -144,32 +143,126 @@ struct MyBox<T>(T);
 //     println!("--------------------------------")
 // }
 
-use std::rc::Rc;
-use crate::List::{Cons, Nil};
+// use std::rc::Rc;
+// use crate::List::{Cons, Nil};
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil
+// }
+
+// fn main () {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     let b = Cons(3, Rc::clone(&a));
+//     let c = Cons(4, Rc::clone(&a));
+//     println!("Reference count after creating b: {}", Rc::strong_count(&a));
+//     println!("Reference count after creating c: {}", Rc::strong_count(&a));
+//     {
+//         let m = Cons(4, Rc::clone(&a));
+//         let m = Cons(4, Rc::clone(&a));
+//         let m = Cons(4, Rc::clone(&a));
+//         let m = Cons(4, Rc::clone(&a));
+//         let m = Cons(4, Rc::clone(&a));
+//         let m = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     drop(b);
+//     println!("Reference count after creating c: {}", Rc::strong_count(&a));
+//     drop(c);
+//     println!("Reference count after creating b: {}", Rc::strong_count(&a));
+//     drop(a);
+// }
+
+// use std::{cell::RefCell, rc::Rc};
+
+// fn main() {
+//     let x = RefCell::new(5);
+
+//     let mut mul_ref: std::cell::RefMut<'_, i32> = x.borrow_mut();
+//     *mul_ref += 1;
+
+//     let imm_ref = x.borrow();
+//     println!("Immutable Reference: {}", *imm_ref)
+// }
+
+// fn main() {
+//     let x = RefCell::new(42);
+
+//     // Attempt to break borrowing rules
+//     let imm_ref = x.borrow();
+//     let mut_ref = x.borrow_mut(); // This will panic at runtime
+// }
+
+// pub trait Messenger {
+//     fn send(&self, msg: &str);
+// }
+
+// pub struct EmailMessenger;
+
+// impl Messenger for EmailMessenger {
+//     fn send(&self, msg: &str) {
+//         println!("Sending message: {}", msg);
+//     }
+// }
+
+// pub struct LimitTracker<'a, T: Messenger>  {
+//     messenger: &'a T,
+//     value: usize,
+//     max: usize,
+// }
+
+// impl <'a, T> LimitTracker<'a, T> where T: Messenger {
+//     pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+//         LimitTracker {
+//             messenger,
+//             value: 0,
+//             max,
+//         }
+//     }
+
+//     pub fn set_value(&mut self, value: usize) {
+//         self.value = value;
+//         let percentage_of_value = 
+//     } 
+// }
+
+// struct MyStruct {
+//     data: i32,
+//     shared: Rc<RefCell<MyStruct>>
+// }
+
+// fn main() {
+//     let my_data = Rc::new([10, Rc::new(Nil)]);
+// }
+
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil
 }
 
-fn main () {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
-    println!("Reference count after creating b: {}", Rc::strong_count(&a));
-    println!("Reference count after creating c: {}", Rc::strong_count(&a));
-    {
-        let m = Cons(4, Rc::clone(&a));
-        let m = Cons(4, Rc::clone(&a));
-        let m = Cons(4, Rc::clone(&a));
-        let m = Cons(4, Rc::clone(&a));
-        let m = Cons(4, Rc::clone(&a));
-        let m = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(.., item) => Some(item),
+            Nil => None
+        }
     }
-    drop(b);
-    println!("Reference count after creating c: {}", Rc::strong_count(&a));
-    drop(c);
-    println!("Reference count after creating b: {}", Rc::strong_count(&a));
-    drop(a);
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
 }
