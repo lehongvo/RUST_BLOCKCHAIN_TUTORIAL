3713e887143722524b2ae21d98efd67a983708fa
RajSu <lehongvi19x@gmail.com>
2024-03-06 12:31:32 +0700

    [VoLH][Blockchain][Rust][String document]

--- rust_by_practice/varibale/src/main.rs
+++ rust_by_practice/varibale/src/main.rs
@@ -3923,39 +3923,175 @@
 // =================================================================
 
 // =================================================================
-use std::fmt;
+// use std::fmt;
 
-// DEFINE a newtype `Pretty` here
-#[derive(Debug)]
-struct Pretty(String);
+// // DEFINE a newtype `Pretty` here
+// #[derive(Debug)]
+// struct Pretty(String);
+
+// fn main() {
+//     let w = Pretty("hello".to_string());
+//     println!("w = {:?}", w);
+// }
+// =================================================================
+
+// =================================================================
+
+// FILL in the blanks and FIX errors
+// 1. Don't use `to_string()`
+// 2. Don't add/remove any code line
+// fn main() {
+//     let mut s = "hello, ".to_string();
+//     s.push_str("world");
+//     s.push('!');
+
+
+//     assert_eq!(s, "hello, world!");
+//     move_ownership(s);
+
+// }
+
+// fn move_ownership(s: String) {
+//     println!("ownership of \"{}\" is moved here!", s)
+// }
+// =================================================================
+
+// =================================================================
+// FILL in the blanks
+// fn main() {  
+//     let mut s = String::from("hello, world");
+ 
+//     let slice1: &str = &s ; // In two ways
+//     assert_eq!(slice1, "hello, world");
+ 
+//     let slice2 = &slice1[0..5];
+//     assert_eq!(slice2, "hello");
+ 
+//     let mut slice3 = s.to_owned();
+//     slice3.push('!');
+//     assert_eq!(slice3, "hello, world!");
+ 
+//     println!("Success!");
+//  }
+ 
+// =================================================================
+// Question: how many heap allocations are happening here?
+// Your answer: 
+// fn main() {  
+//     // Create a String type based on `&str`
+//     // The type of string literals is `&str`
+//    let s: String = String::from("hello, world!");
+
+//    // Create a slice point to String `s`
+//    let slice: &str = &s;
+
+//    // Create a String type based on the recently created slice
+//    let s: String = slice.to_string();
+
+//    assert_eq!(s, "hello, world!");
+
+//    println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+
+// FILL in the blank and FIX errors
+// fn main() {
+//     let s = String::from("hello, 世界");
+//     let slice1: &str = &s[0..1]; //tips: `h` only takes 1 byte in UTF8 format
+//     assert_eq!(slice1, "h");
+
+//     let slice2 = &s[7..10]; // Tips: `中`  takes 3 bytes in UTF8 format
+//     assert_eq!(slice2, "世");
+    
+//     // Iterate through all chars in s
+//     for (i, c) in s.chars().enumerate() {
+//         if i == 7 {
+//             assert_eq!(c, '世')
+//         }
+//     }
+
+//     // println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+// fn main() {
+//    let s = "The 🚀 goes to the 🌑!";
+
+//    let rocket = &s[4..8];
+//    // Will equal "🚀"
+//    println!("Value is {}", rocket)
+// }
+// =================================================================
+
+// =================================================================
+
+// FILL in the blanks
+// fn main() {
+//     let mut s = String::new();
+//     s.push_str("hello");
+
+//     // Some bytes, in a vector
+//     let v = vec![104, 101, 108, 108, 111];
+
+//     // Turn a byte's vector into a String
+//     let s1 = String::from_utf8(v).expect("Failled to convert to String");
+    
+    
+//     assert_eq!(s, s1);
+
+// }
+// =================================================================
+
+// =================================================================
+
+// Modify the code below to print out: 
+// 25
+// 25
+// 25
+// Here, there’s no need to allocate more memory inside the loop.
+// fn main() {
+//     let mut s = String::new();
+//     println!("{}", s.capacity());
+
+//     for _ in 0..2 {
+//         s.push_str("hello");
+//         println!("{}", s.capacity());
+//     }
+
+//     println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+
+// FILL in the blanks
+use std::mem::{self, ManuallyDrop};
 
 fn main() {
-    let w = Pretty("hello".to_string());
-    println!("w = {:?}", w);
+    let story = String::from("Rust By Practice");
+
+    // Prevent automatically dropping of the String's data
+    let mut story = ManuallyDrop::new(story);
+    println!("Value is {:?}", story);
+
+    let ptr = story.as_mut_ptr();
+    let len = story.len();
+    let capacity = story.capacity();
+
+    assert_eq!(16, len);
+
+    // We can rebuild a String out of ptr, len, and capacity. This is all
+    // unsafe because we are responsible for making sure the components are
+    // valid:
+    let s = unsafe { String::from_raw_parts(ptr, len, capacity) };
+
+    assert_eq!(*story, s);
+
+    println!("Success!");
 }
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-// =================================================================
-// =================================================================
-
-// =================================================================
 // =================================================================
 
 // =================================================================
