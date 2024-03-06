029395920107622ff3cae39902b46f3699e6cd97
RajSu <lehongvi19x@gmail.com>
2024-03-06 11:44:48 +0700

    [VoLH][Blockchain][Rust][Generics and Traits]

--- rust_by_practice/varibale/src/main.rs
+++ rust_by_practice/varibale/src/main.rs
@@ -3602,7 +3602,7 @@
 //         container.contains(&number_1, &number_2));
 //     println!("First number: {}", container.first());
 //     println!("Last number: {}", container.last());
-    
+
 //     println!("The difference is: {}", difference(&container));
 // }
 
@@ -3626,55 +3626,313 @@
 //         }
 //     }
 // }
-
-use std::ops::Mul;
-
 // fn main(){
 //     let point = Point{x: 0, y:10} - Point{x: 0, y:11};
 //     println!("Value is {:?}", point);
 // }
 // =================================================================
-#[derive(Debug, PartialEq)]
-struct Point<T> {
-    x: T,
-    y: T
-} 
+// #[derive(Debug, PartialEq)]
+// struct Point<T> {
+//     x: T,
+//     y: T
+// }
 
-impl <T: Mul<Output = T>> Mul for Point<T> {
-    type Output = Self;
+// impl <T: Mul<Output = T>> Mul for Point<T> {
+//     type Output = Self;
 
-    fn mul(self, rhs: Self) -> Self::Output {
-        let point = Point {
-            x: self.x * rhs.x,
-            y: self.y * rhs.y
-        };
-        return point;
-    }
-}
+//     fn mul(self, rhs: Self) -> Self::Output {
+//         let point = Point {
+//             x: self.x * rhs.x,
+//             y: self.y * rhs.y
+//         };
+//         return point;
+//     }
+// }
+// fn main() {
+//     let point = Point{x: 3, y: 10} * Point{x: 4, y: 9};
+//     println!("Value is: {:?}", point);
+// }
+// =================================================================
+
+// =================================================================
+// use std::ops::{Add, Mul};
+
+// #[derive(Debug)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T: Add<Output = T>> Add for Point<T> {
+//     type Output = Self;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         let new_point = Point {
+//             x: self.x + rhs.x,
+//             y:  self.y + rhs.y
+//         };
+//         return new_point;
+//     }
+// }
+
+// fn main() {
+//     let p1 = Point {x: 1, y: 2};
+//     let p2 = Point {x: 3, y: 43};
+//     let p2 = p1 + p2;
+//     println!("Value is {:?}", p2);
+// }
+// =================================================================
+
+// #[derive(Debug)]
+// struct Container(i32, i32);
+
+// trait Contains<A, B> {
+//     fn contains(&self, number_1: &A, number_2: &B) -> bool;
+//     fn first(&self) -> i32;
+//     fn last(&self) -> i32;
+// }
+
+// impl Contains<i32, i32> for Container {
+//     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+//         let value = (&self.0 == number_1) && (&self.1 == number_2);
+//         return value;
+//     }
+//     // Grab the first number.
+//     fn first(&self) -> i32 {
+//         self.0
+//     }
+
+//     // Grab the last number.
+//     fn last(&self) -> i32 {
+//         self.1
+//     }
+// }
+
+// fn difference<A, B, C: Contains<A, B>>(container: &C) -> i32 {
+//     container.last() - container.first()
+// }
+
+// fn main() {
+//     let number_1 = 3;
+//     let number_2 = 10;
+
+//     let container = Container(number_1, number_2);
+
+//     println!(
+//         "Does container contain {} and {}: {}",
+//         &number_1,
+//         &number_2,
+//         container.contains(&number_1, &number_2)
+//     );
+//     println!("First number: {}", container.first());
+//     println!("Last number: {}", container.last());
+
+//     println!("The difference is: {}", difference(&container));
+// }
+
+// =================================================================
+// use std::ops::Sub;
+
+// #[derive(Debug, PartialEq)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl <T: Sub<Output = T>> Sub for Point<T> {
+//     type Output = Self;
+//     fn sub(self, other: Self) -> Self::Output {
+//         Point {
+//             x: self.x - other.x,
+//             y: self.y - other.y,
+//         }
+//     }
+// }
+
+// fn main() {
+//     assert_eq!(
+//         Point { x: 2, y: 3 } - Point { x: 1, y: 0 },
+//         Point { x: 1, y: 3 }
+//     );
+
+//     println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+// trait UsernameWidget {
+//     // Get the selected username out of this widget
+//     fn get(&self) -> String;
+// }
+
+// trait AgeWidget {
+//     // Get the selected age out of this widget
+//     fn get(&self) -> u8;
+// }
+
+// // A form with both a UsernameWidget and an AgeWidget.
+// #[derive(Debug)]
+// struct Form {
+//     username: String,
+//     age: u8,
+// }
+
+// impl UsernameWidget for Form {
+//     fn get(&self) -> String {
+//         self.username.clone()
+//     }
+// }
+
+// impl AgeWidget for Form {
+//     fn get(&self) -> u8 {
+//         self.age
+//     }
+// }
+
+// fn main() {
+//     let form = Form{
+//         username: "rustacean".to_owned(),
+//         age: 28,
+//     };
+
+//     // If you uncomment this line, you'll get an error saying 
+//     // "multiple `get` found". Because, after all, there are multiple methods
+//     // named `get`.
+//     // println!("{}", form.get());
+    
+//     let username = UsernameWidget::get(&form);
+//     println!("Value is {}", username);
+//     assert_eq!("rustacean".to_owned(), username);
+//     let age = AgeWidget::get(&form); // You can also use `<Form as AgeWidget>::get`
+//     assert_eq!(28, age);
+
+//     println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+// trait Pilot {
+//     fn fly(&self) -> String;
+// }
+// trait Wizard {
+//     fn fly(&self) -> String;
+// }
+
+// struct Human;
+
+// impl Pilot for Human {
+//     fn fly(&self) -> String {
+//         String::from("This is your captain speaking.")
+//     }
+// }
+
+// impl Wizard for Human {
+//     fn fly(&self) -> String {
+//         String::from("Up!")
+//     }
+// }
+
+// impl Human {
+//     fn fly(&self) -> String {
+//         String::from("*waving arms furiously*")
+//     }
+// }
+
+// fn main() {
+//     let person = Human;
+
+//     assert_eq!(Pilot::fly(&person), "This is your captain speaking.");
+//     assert_eq!(Wizard::fly(&person), "Up!");
+
+//     assert_eq!(person.fly(), "*waving arms furiously*");
+
+//     println!("Success!");
+// }
+// =================================================================
+
+// =================================================================
+// trait Person {
+//     fn name(&self) -> String;
+// }
+
+// trait Student: Person {
+//     fn university(&self) -> String;
+// }
+
+// trait Programmer {
+//     fn fav_language(&self) -> String;
+// }
+
+// trait CompSciStudent: Programmer + Student {
+//     fn git_username(&self) -> String;
+// }
+
+// fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+//     format!(
+//         "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+//         student.name(),
+//         student.university(),
+//         student.fav_language(),
+//         student.git_username()
+//     )
+// }
+
+// struct CSStudent {
+//     name: String,
+//     university: String,
+//     fav_language: String,
+//     git_username: String
+// }
+
+// impl CompSciStudent for CSStudent {
+//     fn git_username(&self) -> String {
+//         self.git_username.clone()
+//     }
+// }
+
+// // Làm tương tự cho các phương thức của trait Person, Student và Programmer
+// impl Person for CSStudent {
+//     fn name(&self) -> String {
+//         self.name.clone()
+//     }
+// }
+
+// impl Student for CSStudent {
+//     fn university(&self) -> String {
+//         self.university.clone()
+//     }
+// }
+
+// impl Programmer for CSStudent {
+//     fn fav_language(&self) -> String {
+//         self.fav_language.clone()
+//     }
+// }
+
+
+// fn main() {
+//     let student = CSStudent {
+//         name: "Sunfei".to_string(),
+//         university: "XXX".to_string(),
+//         fav_language: "Rust".to_string(),
+//         git_username: "sunface".to_string()
+//     };
+
+//     println!("{}", comp_sci_student_greeting(&student));
+// }
+// =================================================================
+
+// =================================================================
+use std::fmt;
+
+// DEFINE a newtype `Pretty` here
+#[derive(Debug)]
+struct Pretty(String);
+
 fn main() {
-    let point = Point{x: 3, y: 10} * Point{x: 4, y: 9};
-    println!("Value is: {:?}", point);
+    let w = Pretty("hello".to_string());
+    println!("w = {:?}", w);
 }
-
-// =================================================================
-
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-
-// =================================================================
-// =================================================================
-// =================================================================
 // =================================================================
 
 // =================================================================
