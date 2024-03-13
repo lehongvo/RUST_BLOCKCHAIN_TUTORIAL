0dc7021709f9e9d6d45b47d570943d044cb0e186
RajSu <=>
2024-03-13 12:54:35 +0700

    [VolH][Blockchain][Rust][

--- solana/airdrop-project/index.js
+++ solana/airdrop-project/index.js
@@ -24,40 +24,58 @@
 
 // getWalletBalance();
 
-
 const {
     Connection,
     PublicKey,
     clusterApiUrl,
     Keypair,
     LAMPORTS_PER_SOL
-} = require("@solana/web3.js")
+} = require('@solana/web3.js')
 
-const wallet = new Keypair()
+const rpc = 'https://solana-devnet.g.alchemy.com/v2/lsOoSzMo0aZwMWxeHXgqBzhogj6TRUNg'
+// const wallet = new Keypair()
 
-const publicKey = new PublicKey(wallet._keypair.publicKey)
-const secretKey = wallet._keypair.secretKey
+// const publicKey = new PublicKey(wallet._keypair.publicKey)
+// const secretKey = wallet._keypair.secretKey
 
-const getWalletBalance = async() => {
+const getWalletBalance = async () => {
     try {
-        // const rpc = "https://solana-devnet.g.alchemy.com/v2/lsOoSzMo0aZwMWxeHXgqBzhogj6TRUNg";
-        const publicKey =  new PublicKey("4TxsVJKRRrfLowoKWjvT4As2TQDuAhWibXxZrQGQcqV9");
-        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        console.log('Start ==')
+
+        const publicKey = new PublicKey(
+            '4TxsVJKRRrfLowoKWjvT4As2TQDuAhWibXxZrQGQcqV9'
+        )
+        const connection = new Connection(rpc, 'confirmed')
         const walletBalance = await connection.getBalance(publicKey)
         console.log(`Wallet balance is ${walletBalance}`)
-    } catch(err) {
+    } catch (err) {
         console.error(err)
     }
 }
 
-const airDropSol = async() => {
+const airDropSol = async () => {
     try {
-        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
-        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        console.log('airDropSol: ', LAMPORTS_PER_SOL)
+        const publicKey = new PublicKey(
+            '4TxsVJKRRrfLowoKWjvT4As2TQDuAhWibXxZrQGQcqV9'
+        )
+        const connection = new Connection(rpc, 'confirmed')
+        const fromAirDropSignature = await connection.requestAirdrop(
+            publicKey,
+            2 * LAMPORTS_PER_SOL
+        )
+        const latestBlockHash = await connection.getLatestBlockhash()
+        const tx = await connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: fromAirDropSignature
+        })
+        console.log(tx, fromAirDropSignature)
     } catch (error) {
-        console.error(err)
+        console.error(error)
     }
 }
+
+airDropSol()
 
 // getWalletBalance();
-
